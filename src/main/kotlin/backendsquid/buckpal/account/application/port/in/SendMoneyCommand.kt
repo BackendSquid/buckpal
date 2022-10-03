0f758412e4d99dcb69567aca2402acd273e1a623
K <kihyuk.sung@outlook.kr>
2022-10-03 14:01:54 +0900

    feat: SendMoneyCommand validation
    - validation annotation 넣지 않은 이유는 kotlin not null
    로 컴파일러 검증 가능
    - money가 양수인 것은 init 에 추가

--- src/main/kotlin/backendsquid/buckpal/account/application/port/in/SendMoneyCommand.kt
+++ src/main/kotlin/backendsquid/buckpal/account/application/port/in/SendMoneyCommand.kt
@@ -3,6 +3,9 @@ package backendsquid.buckpal.account.application.port.`in`
 import backendsquid.buckpal.account.domain.Account
 import backendsquid.buckpal.account.domain.Money
 import backendsquid.buckpal.common.SelfValidating
+import org.hibernate.validator.internal.engine.ConstraintViolationImpl
+import javax.validation.ConstraintViolation
+import javax.validation.ConstraintViolationException
 
 data class SendMoneyCommand(
     val sourceAccountId: Account.AccountId,
@@ -11,6 +14,10 @@ data class SendMoneyCommand(
 ): SelfValidating<SendMoneyCommand>() {
 
     init {
+        if (!money.isPositive()) {
+            throw IllegalArgumentException("Money must be positive")
+        }
+
         validateSelf()
     }
 
