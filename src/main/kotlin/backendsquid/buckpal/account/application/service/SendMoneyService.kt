7dd00ee7dfc44bb44025dba3aabe905be5ad844c
K <kihyuk.sung@outlook.kr>
2022-10-03 14:23:21 +0900

    feat: SendMoneyService 구현
    - 기본적인 동작 구조 구현

--- src/main/kotlin/backendsquid/buckpal/account/application/service/SendMoneyService.kt
+++ src/main/kotlin/backendsquid/buckpal/account/application/service/SendMoneyService.kt
@@ -5,6 +5,7 @@ import backendsquid.buckpal.account.application.port.`in`.SendMoneyUseCase
 import backendsquid.buckpal.account.application.port.out.AccountLock
 import backendsquid.buckpal.account.application.port.out.LoadAccountPort
 import backendsquid.buckpal.account.application.port.out.UpdateAccountStatePort
+import java.time.LocalDateTime
 import javax.transaction.Transactional
 
 @Transactional
@@ -13,7 +14,35 @@ class SendMoneyService(
     private val accountLock: AccountLock,
     private val updateAccountStatePort: UpdateAccountStatePort,
 ): SendMoneyUseCase {
-    override fun sendMoney(command: SendMoneyCommand) {
-        TODO("Not yet implemented")
+    override fun sendMoney(command: SendMoneyCommand): Boolean {
+        val baseline = LocalDateTime.now().minusDays(10)
+        val sourceAccount = loadAccountPort.loadAccount(
+            accountId = command.sourceAccountId,
+            baseLineDate = baseline,
+        )
+        val targetAccount = loadAccountPort.loadAccount(
+            accountId = command.targetAccountId,
+            baseLineDate = baseline,
+        )
+
+        accountLock.lockAccount(sourceAccount.id)
+        if (!sourceAccount.withdraw(money = command.money, targetAccountId = targetAccount.id)) {
+            accountLock.releaseAccount(sourceAccount.id)
+            return false
+        }
+
+        accountLock.lockAccount(targetAccount.id)
+        if (!targetAccount.deposit(money = command.money, sourceAccountId = sourceAccount.id)) {
+            accountLock.releaseAccount(targetAccount.id)
+            return false
+        }
+
+        updateAccountStatePort.updateActivities(sourceAccount)
+        updateAccountStatePort.updateActivities(targetAccount)
+
+        accountLock.releaseAccount(sourceAccount.id)
+        accountLock.releaseAccount(targetAccount.id)
+
+        return true
     }
 }
