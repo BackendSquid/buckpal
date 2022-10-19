d018cfdc2c8ae10ad74029c2a00106f22999bdd4
K <kihyuk.sung@outlook.kr>
2022-10-19 21:12:16 +0900

    feat: 테스트용 데이터 추가
    - data.sql 추가
    - jpa 초기화 후 data.sql 실행하도록 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO account (id) VALUES (1), (2);
+INSERT INTO activity (`timestamp`, owner_account_id, source_account_id, target_account_id, amount)
+VALUES ('2022-10-01 15:00:00', 1, 2, 1, 100);
